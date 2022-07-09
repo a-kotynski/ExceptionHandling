f4fa8958e6d23ace32ba8f740234108fa3b28109
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-07-09 09:00:31 +0200

    Add project files.

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://www.youtube.com/watch?v=vn7_Ae8CWsE
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("Enter a value of a: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a value of b: ");
+            b = int.Parse(Console.ReadLine());
+
+            c = a / b;
+            Console.WriteLine("Division of a by b is: " + c);
+        }
+    }
+}
